9b892a4b8c3bcde074f438d429ad40eba6ebbf8b
Danylo <40806031+Galyckyj@users.noreply.github.com>
2024-12-19 03:30:28 +0200

    Update

--- app/components/PopularBooks/PopularBooks.tsx
+++ app/components/PopularBooks/PopularBooks.tsx
@@ -52,8 +52,8 @@ export function EmblaCarousel() {
         <div className="embla__container flex gap-6">
           {books.map((book) => (
             <div key={book.id} className="embla__slide bookslider flex flex-col">
-              <div className="image  w-[135px] bg-[#D9D9D9] shadow-md rounded-xl">
-                <img src={book.image} alt={book.title} className='w-[100%] rounded-xl'/>
+              <div className="image  w-[135px] rounded-xl">
+                <img src={book.image} alt={book.title} className='w-[100%] shadow-md rounded-xl'/>
                 <div className="booktitle">
                 <p className='text-[#A7BAD2] font-normal'>{book.author}</p>
                 <p className='text-[#30435C] font-semibold'>{book.title}</p>
